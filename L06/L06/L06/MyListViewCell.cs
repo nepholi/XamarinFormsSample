0f555fd55f9b6b11ab7bb7b452c3c71e3dca7b3b
Pow Lee <powlee@i3d.io>
2016-04-26 12:05:48 +0800

    L06: use ImageButton on Anrdoid, normal button on IOS

--- L06/L06/L06/MyListViewCell.cs
+++ L06/L06/L06/MyListViewCell.cs
@@ -12,7 +12,7 @@ namespace L05_2
 	public class MyListViewCell : ViewCell
 	{
 		private class MyListViewCellButton : Button { }
-		public MyListViewCell()
+        public MyListViewCell()
 		{
 			var nameLabel = new Label
 			{
@@ -31,33 +31,43 @@ namespace L05_2
 			};
 			addressLabel.SetBinding(Label.TextProperty, new Binding("Address"));
 
-			var callButton = new MyListViewCellButton
-			{
-				Text = "Call",
-				BackgroundColor = Color.Green,
-				
-				HorizontalOptions = LayoutOptions.EndAndExpand,
-				VerticalOptions = LayoutOptions.CenterAndExpand,
-				
-			};
-			callButton.SetBinding(Button.CommandParameterProperty, new Binding("Tel"));
-		    callButton.Clicked +=async (sender, e) =>
-		    {
-		        var b = (Button) sender;
-		        var t = b.CommandParameter;
-		        Debug.WriteLine("clicked" + t);
-#if (DEBUG)
+            var callImageButton = new Image
+            {
+                Source = "phone.png",
+                HorizontalOptions = LayoutOptions.EndAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+            callImageButton.SetBinding(Image.BindingContextProperty, new Binding("Tel"));
+            var tapImage = new TapGestureRecognizer();
+            tapImage.Tapped += (sender, e) =>
+            {
+                var i = (Image)sender;
+                var t = i.BindingContext;
+                Device.OpenUri(new Uri($"tel:{t}"));
+            };
+            callImageButton.GestureRecognizers.Add(tapImage);
 
-                if (Device.OS == TargetPlatform.iOS)
-		        {
-                    await ((((b.ParentView as StackLayout).ParentView as ListView).ParentView as StackLayout).ParentView as ContentPage).DisplayAlert("Alert",$"iOS模擬器無法打電話\r點擊的電話為:{t}","OK");
-		            return;
-		        }
+            var callButton = new MyListViewCellButton
+            {
+                Text = "Call",
+                BackgroundColor = Color.Green,
+                HorizontalOptions = LayoutOptions.EndAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+            };
+            callButton.SetBinding(Button.CommandParameterProperty, new Binding("Tel"));
+            callButton.Clicked += async (sender, e) =>
+             {
+                var b = (Button)sender;
+                var t = b.CommandParameter;
+                Debug.WriteLine("clicked" + t);
+#if (DEBUG)
+                await ((((b.ParentView as StackLayout).ParentView as ListView).ParentView as StackLayout).ParentView as ContentPage).DisplayAlert("Alert", $"iOS模擬器無法打電話\r點擊的電話為:{t}", "OK");
+                return;
 #endif
                 Device.OpenUri(new Uri($"tel:{t}"));
-		    };
+             };
 
-			View = new StackLayout
+            View = new StackLayout
 			{
 				Orientation = StackOrientation.Horizontal,
 				HorizontalOptions = LayoutOptions.StartAndExpand,
@@ -65,13 +75,17 @@ namespace L05_2
 				Padding = new Thickness(6, 10, 6, 10),
 				Children = {
 					new StackLayout {
-						WidthRequest = 280,
+						WidthRequest = App.ScreenWidth*0.85,
 						Orientation = StackOrientation.Vertical,
 						Children = { nameLabel, addressLabel }
 					},
-					callButton
-				}
-			};
+#if __IOS__
+                    callButton
+#else
+                    callImageButton
+#endif
+                }
+            };
 
 			//透過各平台自訂的程式碼抓取不同平台所獲得的螢幕寬度。
 			Debug.WriteLine(App.ScreenWidth);
